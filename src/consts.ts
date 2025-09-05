f3a636b169624b49d859b8f7182694e386ae12d9
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-09-05 23:22:39 +0530

    init

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "FH Course";
+export const SITE_DESCRIPTION =
+  "This course will teach you how to setup a quick CMS using Pages CMS";
